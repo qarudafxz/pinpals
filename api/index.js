91d95b3d028e6e895bf3f43c426654205816676f
qaruda <70809588+francistinao@users.noreply.github.com>
2023-04-04 17:23:09 +0800

    Make api prefix route

--- api/index.js
+++ api/index.js
@@ -15,8 +15,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/auth", userRouter);
-app.use("/pins", pinsRouter);
+app.use("/api/auth", userRouter);
+app.use("/api/pins", pinsRouter);
 
 mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
