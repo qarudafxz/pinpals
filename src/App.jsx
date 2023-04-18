d981559f110ef7e9f7e67d7bab51baeb0c077d1d
qaruda <70809588+francistinao@users.noreply.github.com>
2023-04-18 08:38:02 +0800

    added analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 
 import Home from './pages/Home'
 import Login from './pages/Login'
@@ -17,6 +18,7 @@ function App() {
           <Route path="/pins/all/" element={<Pins />} />
         </Routes>
       </Router>
+      <Analytics />
     </div>
   )
 }
